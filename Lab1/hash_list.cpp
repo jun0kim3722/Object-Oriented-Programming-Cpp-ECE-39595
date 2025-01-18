e7f619ce4ba74c75677fcd9df707d2c4eda1b146
jun0kim3722 <jun0kim0329@gmail.com>
2025-01-18 15:21:19 -0500

    init

---
+++ Lab1/hash_list.cpp
@@ -0,0 +1,106 @@
+#include "hash_list.h"
+
+hash_list::hash_list() {
+    head = nullptr;
+    iter_ptr = nullptr;
+    size = 0;
+}
+
+/**-----------------------------------------------------------------------------------
+ * START Part 1
+ *------------------------------------------------------------------------------------*/
+
+void hash_list::insert(int key, float value) {
+    if (size == 0) {
+        head = new node(key, value);
+        size += 1;
+    }
+    else {
+        bool is_updated = false;
+        node *prev_node = head;
+        for (node *end_node = head->next; end_node != nullptr; end_node = end_node->next) {
+            if (end_node -> key == key) {
+                end_node -> value = value;
+                is_updated = true;
+            }
+            prev_node = end_node;
+        }
+
+        if (!is_updated) {
+            node *new_node = new node(key, value);
+            prev_node -> next = new_node;
+            size += 1;
+        }
+    }
+}
+
+std::optional<float> hash_list::get_value(int key) const {
+    for (node *end_node = head; end_node != nullptr; end_node = end_node->next) {
+        if (end_node -> key == key) {
+            return end_node -> value;
+        }
+    }
+
+    return std::nullopt;
+}
+
+bool hash_list::remove(int key) {
+    node *prev_node = nullptr;
+    for (node *end_node = head; end_node != nullptr; end_node = end_node->next) {
+        if (end_node -> key == key) {
+            if (prev_node != nullptr) {
+                prev_node -> next = end_node -> next;
+            }
+            else {
+                head = end_node -> next;
+            }
+            delete end_node;
+            size -= 1;
+
+            return true;
+        }
+        prev_node = end_node;
+    }
+    
+    return false;
+}
+
+size_t hash_list::get_size() const {
+    return size;
+}
+
+hash_list::~hash_list() {
+    while (head != nullptr) {
+        node *next = head->next;
+        delete head;
+        head = next;
+    }
+    size = 0;
+}
+
+/**-----------------------------------------------------------------------------------
+ * END Part 1
+ *------------------------------------------------------------------------------------*/
+
+
+/**-----------------------------------------------------------------------------------
+ * START Part 2
+ *------------------------------------------------------------------------------------*/
+
+hash_list::hash_list(const hash_list &other) {}
+
+hash_list &hash_list::operator=(const hash_list &other) { return *this; }
+
+void hash_list::reset_iter() {}
+
+
+void hash_list::increment_iter() {}
+
+
+std::optional<std::pair<const int *, float *>> hash_list::get_iter_value() { return std::nullopt; }
+
+
+bool hash_list::iter_at_end() { return false; }
+/**-----------------------------------------------------------------------------------
+ * END Part 2
+ *------------------------------------------------------------------------------------*/
---
+++ Lab1/main.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+
+#include "hash_list.h"
+
+int trace_1();
+int trace_2();
+int trace_3();
+int trace_4();
+int trace_5();
+
+#ifdef PART2 // Only test iterators in part 2
+void iterator_example(hash_list &list)
+{
+    /** We have to reset the iterator before we use it */
+    list.reset_iter();
+
+    /** We want to iterate over all nodes, so keep iterating until we're at the end */
+    while (!list.iter_at_end())
+    {
+        /** Get pointers to key/value pairs */
+        std::optional<std::pair<const int *, float *>> node_values = list.get_iter_value();
+
+        /** Increment the VALUE. Note that the key remains unchanged */
+        (*node_values.value().second)++;
+
+        std::cout << "Key: " << *node_values.value().first
+                  << " and value: " << *node_values.value().second << std::endl;
+
+        /** Go to the next element */
+        list.increment_iter();
+    }
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+
+#ifdef PART1
+    hash_list list;
+
+    if (list.get_size() != 0)
+    {
+        std::cout << "Invalid size" << std::endl;
+        exit(1);
+    }
+
+    list.insert(3, 3);
+    if (list.get_size() != 1)
+    {
+        std::cout << "Invalid size" << list.get_size() << std::endl;
+        exit(1);
+    }
+
+    list.insert(4, 4);
+    if (list.get_size() != 2)
+    {
+        std::cout << "Invalid size" << list.get_size() << std::endl;
+        exit(1);
+    }
+
+    list.insert(5, 5);
+    if (list.get_size() != 3)
+    {
+        std::cout << "Invalid size" << list.get_size() << std::endl;
+        exit(1);
+    }
+
+    list.insert(6, 6);
+    if (list.get_size() != 4)
+    {
+        std::cout << "Invalid size" << list.get_size() << std::endl;
+        exit(1);
+    }
+
+    if (!list.get_value(3).has_value())
+    {
+        std::cout << "expected 3 to be in list but it wasn't" << std::endl;
+        exit(1);
+    }
+
+    if (!list.get_value(4).has_value())
+    {
+        std::cout << "expected 4 to be in list but it wasn't" << std::endl;
+        exit(1);
+    }
+
+    if (!list.get_value(5).has_value())
+    {
+        std::cout << "expected 5 to be in list but it wasn't" << std::endl;
+        exit(1);
+    }
+
+    if (!list.get_value(6).has_value())
+    {
+        std::cout << "expected 6 to be in list but it wasn't" << std::endl;
+        exit(1);
+    }
+
+    
+
+    // if (!list.remove(3))
+    // {
+    //     std::cout << "Failed to remove 3 from list" << std::endl;
+    //     exit(1);
+    // }
+
+    // if (list.get_value(3).has_value())
+    // {
+    //     std::cout << "Unexpected 3 in list" << std::endl;
+    //     exit(1);
+    // }
+
+    if (!list.remove(4))
+    {
+        std::cout << "Failed to remove 4 from list" << std::endl;
+        exit(1);
+    }
+
+    if (list.get_value(4).has_value())
+    {
+        std::cout << "Unexpected 4 in list" << std::endl;
+        exit(1);
+    }
+
+    if (list.get_size() != 3)
+    {
+        std::cout << "Invalid size" << list.get_size() << std::endl;
+        exit(1);
+    }
+
+    if (!list.remove(5))
+    {
+        std::cout << "Failed to remove 5 from list" << std::endl;
+        exit(1);
+    }
+
+    if (list.get_value(5).has_value())
+    {
+        std::cout << "Unexpected 5 in list" << std::endl;
+        exit(1);
+    }
+    if (list.get_size() != 2)
+    {
+        std::cout << "Invalid size" << list.get_size() << std::endl;
+        exit(1);
+    }
+
+
+#endif
+
+#ifdef PART2
+    iterator_example(list);
+#endif
+
+}
